60b63e83fbf37f5ec870e1aa6c177518427df8bc
Jhon <jhoncasique04@hotmail.com>
2018-12-21 17:29:53 +0100

    commit

--- Note.java
+++ Note.java
@@ -1,5 +1,8 @@
 package com.jhonisaac.todolist;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Note {
     //Variables
     private String mTitle;
@@ -7,6 +10,44 @@ public class Note {
     private boolean mIdea;
     private boolean mTodo;
     private boolean mImportant;
+
+    private static final String JSON_TITLE = "title";
+    private static final String JSON_DESCRIPTION = "description";
+    private static final String JSON_IDEA = "idea";
+    private static final String JSON_TODO = "todo";
+    private static final String JSON_IMPORTANT = "important";
+
+    public Note(){
+
+    }
+
+    //Constructor to created a note from JSONObject
+    public Note (JSONObject jo) throws JSONException {
+        mTitle = jo.getString(JSON_TITLE);
+        mDescription = jo.getString(JSON_DESCRIPTION);
+        mIdea = jo.getBoolean(JSON_IDEA);
+        mTodo = jo.getBoolean(JSON_TODO);
+        mImportant = jo.getBoolean(JSON_IMPORTANT);
+    }
+
+    //Serialize the variables from Note and convert to a JsonObject.
+
+    public JSONObject convertNoteToJson() throws JSONException {
+
+        JSONObject jo = new JSONObject();
+
+        jo.put(JSON_TITLE, mTitle);
+        jo.put(JSON_DESCRIPTION, mDescription);
+        jo.put(JSON_IDEA, mIdea);
+        jo.put(JSON_TODO, mTodo);
+        jo.put(JSON_IMPORTANT, mImportant);
+
+        return jo;
+    }
+
+
+
+
 
     public String getTitle() {
         return mTitle;
