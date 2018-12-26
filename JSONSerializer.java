3a475e346dd02972e8d321b7eaac4059233f43d8
Jhon <jhoncasique04@hotmail.com>
2018-12-26 22:47:29 +0100

    Serializer

---
+++ JSONSerializer.java
@@ -0,0 +1,17 @@
+package com.jhonisaac.todolist;
+
+import android.content.Context;
+
+public class JSONSerializer {
+
+    private String mFileName;
+    private Context mContext;
+
+    public JSONSerializer(String fileName , Context context) {
+        this.mFileName = fileName;
+        this.mContext = context;
+    }
+
+    
+
+}
